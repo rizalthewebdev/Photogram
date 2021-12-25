4ac949e8d126e5ba2fefd0c3a17613077c221497
rzlTrimelty <rzl.trimelty@gmail.com>
2021-12-25 10:42:26 +0700

    Sidebar & Navbar Done

---
+++ src/components/PinDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PinDetails = () => {
+    return (
+        <div>
+            Pins Details
+        </div>
+    )
+}
+
+export default PinDetails
---
+++ src/container/Home.js
@@ -0,0 +1,67 @@
+import React, { useState, useRef, useEffect} from 'react'
+import { HiMenuAlt1 } from 'react-icons/hi'
+import { MdClose } from 'react-icons/md'
+import { Link, Routes, Route } from 'react-router-dom'
+
+
+import { Sidebar, Login, UserProfile } from '../components'
+import Pins from '../container/Pins'
+import { userQuery } from '../utils/data'
+import { client } from '../client'
+import logo from '../assets/logo.png'
+
+const Home = () => {
+
+    const [toggleSidebar, setToggleSidebar] = useState(false)
+    const [user, setUser] = useState(null)
+    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+    const scrollRef = useRef(null)
+
+    useEffect(() => {
+        const query = userQuery(userInfo?.googleId)
+        
+        client.fetch(query)
+            .then((data) => {
+                setUser(data[0])
+            })
+    }, [user])
+
+    useEffect(() => {
+        scrollRef.current.scrollTo(0, 0)
+    }, []);
+
+    return (
+        <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+            <div className="hidden md:flex h-screen flex-initial">
+                <Sidebar user={ user && user} />
+            </div>
+            <div className="flex md:hidden flex-row">
+                <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+                    <HiMenuAlt1 fontSize={35} className="cursor-pointer" onClick={() => setToggleSidebar(true) } />
+                    <Link to='/'>
+                        <img src={logo} alt="Logo" className="w-28" />
+                    </Link>
+                    <Link to={`user-profile/${user?._id}`}>
+                        <img src={user?.image} alt="Profile Image" width={35} className="rounded-full" />
+                    </Link>
+                </div>
+                {toggleSidebar && (
+                    <div className="fixed w-3/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                        <div className="absolute w-full flex justify-end items-center p-2">
+                            <MdClose fontSize={35} className="cursor-pointer mx-3" onClick={() => setToggleSidebar(false)} />
+                        </div>
+                        <Sidebar user={ user && user} closeToggle={setToggleSidebar} />
+                    </div>
+                )}
+            </div>
+            <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+                <Routes>
+                    <Route path="/user-profile/:userId" element={<UserProfile />} />
+                    <Route path="/*" element={<Pins user={ user && user } />} />
+                </Routes>
+            </div>
+        </div>
+    )
+}
+
+export default Home
